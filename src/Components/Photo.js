cb1ecb216c37b300692b39328f9fef455098436b
chetanpatil414 <chetanpatil414@gmail.com>
2023-12-09 19:56:36 +0530

    new changes

---
+++ src/Components/Photo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Photo extends React.Component {
+  render() {
+    const post = this.props.post;
+    return <figure className="figure"></figure>;
+  }
+}
+
+export default Photo;
---
+++ src/Components/Tittle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Tittle extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.tittle}</h1>
+      </div>
+    );
+  }
+}
+
+export default Tittle;
