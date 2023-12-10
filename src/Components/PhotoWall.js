950a3736c1b73b238deaa2bf501be3da43940098
chetanpatil414 <chetanpatil414@gmail.com>
2023-12-10 10:56:13 +0530

    Functional components

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Photo from "./Photo";
 
-class PhotoWall extends React.Component {
-  render() {
-    return (
-      <div className="photoGrid">
-        {this.props.posts.map((post, index) => (
-          <Photo key={index} post={post} />
-        ))}
-      </div>
-    );
-  }
+function PhotoWall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo key={index} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default PhotoWall;
--- src/Components/Tittle.js
+++ src/Components/Tittle.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class Tittle extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.tittle}</h1>
-      </div>
-    );
-  }
+function Tittle(props) {
+  return (
+    <div>
+      <h1>{props.tittle}</h1>
+    </div>
+  );
 }
 
 export default Tittle;
